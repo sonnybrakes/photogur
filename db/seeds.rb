7e4574b0fdff9eb5ff210592d736c1ce9193952e
Michel Frechette <mifrec@hotmail.com>
2016-05-21 14:55:59 -0400

    Making pictures show at /pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Picture.create(
+  title:  'zerohedge',
+  artist: '407,000 Workers Stunned As Pension Fund Proposes 60% Cuts, Treasury Says "Not Enough"',
+  url:    'http://www.zerohedge.com/news/2016-05-21/407000-workers-stunned-pension-fund-proposes-60-cuts-treasury-says-not-enough'
+)
+
+Picture.create(
+  title:  'Beautiful',
+  artist: 'Sarah Shahi',
+  url:    'http://porhomme.com/wp-content/uploads/2009/01/sarah-shahi-7.jpg'
+)
+
+# Picture.create(
+#   title:  'Into the Poppies',
+#   artist: 'John Wilhelm',
+#   url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg'
+# )
